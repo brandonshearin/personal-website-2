496c22345cf9a5ef50aa27f27c5dc42eed12de78
Brandon Shearin <bshearin15@gmail.com>
2024-08-11 13:41:15 -0400

    fix compile errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,7 +28,7 @@ export default function About() {
         About
       </p>
       <p className={davidExtralight.className} style={{ fontSize: "18px" }}>
-        since i have lived in san francisco i have filled five books with
+        {` since i have lived in san francisco i have filled five books with
         writing, and i return to them to revisit all the feelings and the
         confusions and the dreams. all the transient emotions and first-times
         that have come to me. writing is how i wrangle the inner monologue into
@@ -37,34 +37,34 @@ export default function About() {
         each other like our parents did, and their parents did, and before that
         you just chilled in the village and didn't worry about all this muck.
         how is it that the most context i have for the beautiful people in my
-        life is a june photo dump. what a way to live this is.
+        life is a june photo dump. what a way to live this is.`}
       </p>
       <br />
 
       <p className={davidExtralight.className} style={{ fontSize: "28px" }}>
-        im interested that we feel more comfortable sharing pictures than we do
+        {`im interested that we feel more comfortable sharing pictures than we do
         our words. i see many beautiful pictures but not many beautiful words
         and maybe i follow the wrong people but maybe the big companies haven't
-        given us a nice way to share our words.
+        given us a nice way to share our words.`}
       </p>
       <br />
 
       <p className={davidExtralight.className} style={{ fontSize: "18px" }}>
-        i have nearly one thousand follows on instagram at the moment, sixty
+        {`i have nearly one thousand follows on instagram at the moment, sixty
         percent of which are probably from a troll farm in pakistan. the next
         thirty five percent are a mixed bag of people from high school P.E.
         class, or someone i thought was cute at a bar, or a guy i met during
         rush freshman year who i dapped up on campus but haven't dapped in
         years. of the remaining 15 people who actually know me and want to
-        engage over the long distance, this project is for you.
+        engage over the long distance, this project is for you.`}
       </p>
       <br />
 
       <p className={davidExtralight.className} style={{ fontSize: "18px" }}>
-        maybe i am egotistical and actually think someone will read a single
+        {`maybe i am egotistical and actually think someone will read a single
         word but i like to make things and coding this site was fun. i like to
         write too. all photos are ones that i have taken and the code for this
-        site can be found on github{" "}
+        site can be found on github `}
         <a
           href="https://github.com/brandonshearin/personal-website-2"
           target="__blank"
